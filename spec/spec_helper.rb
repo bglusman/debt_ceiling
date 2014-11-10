ba325471e7a5a02a4c4a96822e30043338b0da31
Brian Glusman <bglusman@shutterstock.com>
2014-11-09 20:46:07 -0500

    Add coveralls for test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'coveralls'
+Coveralls.wear!
 RSpec.configure do |config|
   config.before { allow($stdout).to receive(:puts) }
   config.after(:each) { DebtCeiling.configure {|c| c.debt_ceiling = nil; c.reduction_target = nil; c.reduction_date = nil } }
