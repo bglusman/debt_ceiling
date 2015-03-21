e69a256b7e05951cc86df0afeca19a3819f3adbb
Brian Glusman <bglusman@shutterstock.com>
2015-03-21 19:25:29 -0400

    Add travis/code climate integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 require 'coveralls'
 Coveralls.wear!
+
 RSpec.configure do |config|
   config.before { allow($stdout).to receive(:puts) }
   config.after(:each) { DebtCeiling.configure {|c| c.debt_ceiling = nil; c.reduction_target = nil; c.reduction_date = nil } ; DebtCeiling.clear }
