e3473f79f85d7fd0342efdeab5bdf1dd6f420ec6
Brian Glusman <brian@stellaservice.com>
2016-07-02 14:29:29 -0400

    Fix weird spec with possible hack for now

--- spec/archeological_dig_spec.rb
+++ spec/archeological_dig_spec.rb
@@ -24,14 +24,22 @@ describe DebtCeiling::ArcheologicalDig do
 
   context "without redis" do
     before do
+      DebtCeiling.configure {|c| c.memoize_records_in_repo = true }
       allow(fake_source_control).to receive(:read_note_on).with(COMMIT_SHA).and_return COMMIT_SHA_MSG
       allow(fake_source_control).to receive(:add_note_to)
     end
     it 'adds debt for todos with specified value' do
       dig = DebtCeiling::ArcheologicalDig.new('').process
-      expect(dig.records).to include(
+      expect(dig.records.map(&:stringify_keys)).to include(
         "debt" => total_debt, "failed" => failed_condition, "commit" => COMMIT_SHA
         )
     end
   end
 end
+
+class Hash
+  def stringify_keys
+    map {|k, v| [k.to_s, v] }.to_h
+  end
+end
+
