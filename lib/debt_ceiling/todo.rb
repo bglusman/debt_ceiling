201e4129aa31fd575e3508061bb08f7b5d0b2443
Brian Glusman <brian@stellaservice.com>
2016-07-03 23:39:51 -0400

    Some pretty formatting of json output maybe?

--- lib/debt_ceiling/todo.rb
+++ lib/debt_ceiling/todo.rb
@@ -1,13 +1,18 @@
 module DebtCeiling
   class Todo
 
-    def self.debts(debts)
+    def self.json_debts(debts)
       debts.map(&:custom_debt).compact.map(&:todo_report).compact.reduce(&:merge)
     end
 
     def self.output(debts)
-      hash_debts = self.debts(debts)
-      puts JSON.pretty_generate(hash_debts) if hash_debts
+      hash_debts = self.json_debts(debts)
+      puts JSON.pretty_generate(hash_debts)
+        .gsub("{", "")
+        .gsub("}", "")
+        .gsub("\\n", "")
+        .gsub(/^\s+,\s+/, "")
+        .gsub(/^(\s*)"(\d+)":/, "\\1line \\2:") if hash_debts
     end
 
     attr_reader :accounting, :dir, :loaded
